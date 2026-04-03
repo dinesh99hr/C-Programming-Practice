fd45852db767ea085b74646406bfcaccf5ff9789
Dinesh Chauhan <dinu99tankri@gmail.com>
2026-04-03 12:40:37 +0530

    Update palindrome.c

--- basics/strings/palindrome.c
+++ basics/strings/palindrome.c
@@ -25,3 +25,38 @@ else
 return 0;
 }
 
+
+
+----------------------------------------
+## Improved_code_Palindrom
+----------------------------------------
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h> // Needed for tolower()
+
+int main() {
+    char str[100];
+    int i, length;
+    int isPalindrome = 1;
+
+    printf("Enter a word: ");
+    // %99s limits input to 99 characters to prevent buffer overflow
+    scanf("%99s", str);
+
+    length = strlen(str);
+
+    for(i = 0; i < length / 2; i++) {
+        // tolower() ensures 'A' and 'a' are treated as the same character
+        if(tolower(str[i]) != tolower(str[length - i - 1])) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if(isPalindrome)
+        printf("\"%s\" is a palindrome.\n", str);
+    else
+        printf("\"%s\" is not a palindrome.\n", str);
+
+    return 0;
+}
